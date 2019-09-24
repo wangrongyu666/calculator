6f0fdb92a01c616a9c90376d1f4e96d9d86aab36
ZhaokunChen <chenzhaokun@vip.qq.com>
2019-09-24 23:22:30 +0800

    simple calculator

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var valueA: UITextField!
+    @IBOutlet weak var valueB: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func multiply(_ sender: Any)
+    {
+        var a = Double(valueA.text!)!
+        var b = Double(valueB.text!)!
+        
+        var answer = a * b
+        result.text = "The answer is \(answer)"
+    }
+    
+    @IBAction func divide(_ sender: Any)
+    {
+        var a = Double(valueA.text!)!
+        var b = Double(valueB.text!)!
+        
+        var answer = a / b
+        result.text = "The answer is \(answer)"
+    }
+        
+    @IBAction func plus(_ sender: Any)
+    {
+        var a = Double(valueA.text!)!
+        var b = Double(valueB.text!)!
+        
+        var answer = a + b
+        result.text = "The answer is \(answer)"
+    }
+    
+    @IBAction func minus(_ sender: Any)
+    {
+        var a = Double(valueA.text!)!
+        var b = Double(valueB.text!)!
+        
+        var answer = a - b
+        result.text = "The answer is \(answer)"
+    }
+    
 }
 
